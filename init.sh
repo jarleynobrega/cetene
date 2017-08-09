aea4c5ed9d92162c6dc267cf12f15c2b9419ee32
Jarley <jarleynobrega@gmail.com>
2017-08-09 17:44:29 -0300

    atualizacao do script

--- init.sh
+++ init.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-export TERM xterm
-export PATH=$PATH:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/bin:/sbin:/usr/local/nvidia/bin/
+exec export TERM xterm
+exec export PATH=$PATH:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/bin:/sbin:/usr/local/nvidia/bin/
 
-apt-get update -y
-apt-get upgrade -y
+exec apt-get update -y
+exec apt-get upgrade -y
 
 #pip install keras h5py pillow opencv-python scikit-plot
-apt-get install -y python3-tk cython git nano openssh-server
+exec apt-get install -y python3-tk cython git nano openssh-server
 
-sed -i 's/Port 22/Port 2225/' /etc/ssh/sshd_config
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+exec sed -i 's/Port 22/Port 2225/' /etc/ssh/sshd_config
+exec sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
-echo 'root:cetene' | chpasswd
+exec echo 'root:cetene' | chpasswd
 
-/etc/init.d/ssh stop
-sleep 10
-/etc/init.d/ssh start
-sleep 10
+exec /etc/init.d/ssh stop
+exec sleep 10
+exec /etc/init.d/ssh start
+exec sleep 10
 
-exit 0
+#exit 0
 
