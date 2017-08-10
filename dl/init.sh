a0f4f9877bd23062e7f766409aa09c8727fef984
Jarley <jarleynobrega@gmail.com>
2017-08-10 15:43:49 -0300

    atualizacao do script

--- dl/init.sh
+++ dl/init.sh
@@ -12,7 +12,7 @@ sed -i 's/Port 22/Port 2225/' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 # Senha padrao do root do container
-echo 'root:cetene' | chpasswd
+# echo 'root:cetene' | chpasswd
 
 /etc/init.d/ssh stop
 sleep 10
