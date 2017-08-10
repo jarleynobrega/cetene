ac77e1e086373d263fbf643c7306855e31b070f7
Jarley <jarleynobrega@gmail.com>
2017-08-10 11:49:46 -0300

    atualizacao do script

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-export TERM xterm
-export PATH=$PATH:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/bin:/sbin:/usr/local/nvidia/bin/
-
+# Atualizacao dos pacotes da imagem do Ubuntu
 apt-get update -y
 apt-get upgrade -y
 
-#pip install keras h5py pillow opencv-python scikit-plot
+# Instalacao de pacotes adicionais de Python
 apt-get install -y python3-tk cython git nano openssh-server
 
+# Configuracao do servidor SSH para acesso externo
 sed -i 's/Port 22/Port 2225/' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
+# Senha padrao do root do container
 echo 'root:cetene' | chpasswd
 
 /etc/init.d/ssh stop
@@ -19,5 +19,6 @@ sleep 10
 /etc/init.d/ssh start
 sleep 10
 
-#exit 0
+export TERM xterm
+export PATH=$PATH:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/bin:/sbin:/usr/local/nvidia/bin/
 
