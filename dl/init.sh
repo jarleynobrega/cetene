daf540b006f49e2174eb24986cac4593682424f0
Jarley <jarleynobrega@gmail.com>
2017-08-17 16:23:27 -0300

    Inclusão do PyTorch na inicialização do container

--- dl/init.sh
+++ dl/init.sh
@@ -7,7 +7,11 @@ apt-get update -y
 apt-get upgrade -y
 
 # Instalacao de pacotes adicionais de Python
-apt-get install -y python3-tk cython git nano openssh-server
+apt-get install -y python3-tk cython git nano openssh-server libblas-dev liblapack-dev
+
+# Instalacao do PyTorch e torchvision
+pip3 install http://download.pytorch.org/whl/cu80/torch-0.1.12.post2-cp35-cp35m-linux_x86_64.whl
+pip3 install torchvision
 
 # Configuracao do servidor SSH para acesso externo
 sed -i "s/Port 22/Port $1/" /etc/ssh/sshd_config
