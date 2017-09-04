02bb3f3d1678fba570ebb37f865a910793b86e8f
Jarley <jarleynobrega@gmail.com>
2017-09-04 08:55:03 -0300

    inclusao do screen no script

--- snp/init.sh
+++ snp/init.sh
@@ -15,6 +15,9 @@ apt-get install -y libncurses5-dev libbz2-dev liblzma-dev
 # Instalacao do R
 apt-get install -y r-base
 
+# Instalacao do Screen
+apt-get install -y screen
+
 # Instalacao do PyTorch e torchvision
 pip3 install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl 
 pip3 install torchvision
